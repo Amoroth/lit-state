36a2975bc0ad3adab424711c369cb53ff9a03f5d
Amoroth <Amoroth36@gmail.com>
2019-04-12 15:52:44 +0200

    first commit

---
+++ src/lit-state.js
@@ -0,0 +1,26 @@
+import { directive, render } from 'lit-html'
+
+const stateMap = new WeakMap()
+
+const updateDOM = comp => render(comp(), document.body)
+
+export const renderDOM = (entryComponent) => {
+  window.addEventListener('updateDOM', () => updateDOM(entryComponent))
+  updateDOM(entryComponent)
+}
+
+export const statefulDirective = directive((component, state) => (part) => {
+  let myState = stateMap.get(part)
+  if (myState === undefined) {
+    myState = Object.assign({}, state)
+    stateMap.set(part, myState)
+  }
+  const mySetState = (newState) => {
+    stateMap.set(part, { ...myState, ...newState })
+    window.dispatchEvent(new Event('updateDOM'))
+  }
+  part.setValue(component({
+    state: Object.assign({}, myState),
+    setState: mySetState,
+  }))
+})
