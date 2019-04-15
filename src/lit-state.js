7426b136efeb9b7f622c2da1ee302393eb672ff3
Amoroth <Amoroth36@gmail.com>
2019-04-15 16:19:21 +0200

    renderDOM can use state comp or normal

--- src/lit-state.js
+++ src/lit-state.js
@@ -1,26 +1,27 @@
-import { directive, render } from 'lit-html'
+import { directive, render, html } from 'lit-html'
 
 const stateMap = new WeakMap()
 
 const updateDOM = comp => render(comp(), document.body)
 
 export const renderDOM = (entryComponent) => {
-  window.addEventListener('updateDOM', () => updateDOM(entryComponent))
-  updateDOM(entryComponent)
+  let wrapperComponent = entryComponent.prototype ? entryComponent : () => html`${entryComponent}`
+  window.addEventListener('updateDOM', () => updateDOM(wrapperComponent))
+  updateDOM(wrapperComponent)
 }
 
 export const connectState = directive((component, state) => (part) => {
-  let myState = stateMap.get(part)
-  if (myState === undefined) {
-    myState = Object.assign({}, state)
-    stateMap.set(part, myState)
+  let compState = stateMap.get(part)
+  if (compState === undefined) {
+    compState = Object.assign({}, state)
+    stateMap.set(part, compState)
   }
-  const mySetState = (newState) => {
-    stateMap.set(part, { ...myState, ...newState })
+  const compSetState = (newState) => {
+    stateMap.set(part, { ...compState, ...newState })
     window.dispatchEvent(new Event('updateDOM'))
   }
   part.setValue(component({
-    state: Object.assign({}, myState),
-    setState: mySetState,
+    state: Object.assign({}, compState),
+    setState: compSetState,
   }))
 })
